64a219d669514eea8e46f1daf5f3bf438d889e20
nyo <nyo4rdace@gmail.com>
2020-04-22 01:09:04 +0800

    catch merge request event

--- main.go
+++ main.go
@@ -26,17 +26,52 @@ func run() {
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 }
 
+type user struct {
+	Name      string `json:"name"`
+	Username  string `json:"username"`
+	AvatarURL string `json:"avatar_url"`
+}
+
+type attributes struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	State  string `json:"state"`
+	URL    string `json:"url"`
+	Action string `json:"action"`
+}
+
+type webhook struct {
+	ObjectKind string `json:"object_kind"`
+
+	User             user
+	ObjectAttributes *attributes `json:"object_attributes"`
+}
+
 func server() {
 	engine := gin.New()
 
-	engine.POST("/webhook", func(c *gin.Context) {
+	engine.POST("/webhook/raw", func(c *gin.Context) {
 		raw, err := c.GetRawData()
 		if err != nil {
 			c.AbortWithError(400, err)
 			return
 		}
-
 		fmt.Println(string(raw))
+		c.JSON(http.StatusOK, "")
+	})
+
+	engine.POST("/webhook", func(c *gin.Context) {
+		wh := new(webhook)
+		err := c.BindJSON(wh)
+		if err != nil {
+			c.AbortWithError(400, err)
+			return
+		}
+
+		switch wh.ObjectKind {
+		case "merge_request":
+			fmt.Println(fmt.Sprintf("%s %s MR %s", wh.User.Username, wh.ObjectAttributes.State, wh.ObjectAttributes.Title))
+		}
 
 		c.JSON(http.StatusOK, "")
 	})
